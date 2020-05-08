28b55b34e46e77da76dd07647f2b5b774a03ef51
lack-xu <juluoxu@gmail.com>
2020-05-08 17:52:19 +0800

    Change Project From.

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <BlogHeader></BlogHeader>
+  </div>
+</template>
+
+<script>
+import BlogHeader from "./components/BlogHeader.vue";
+
+export default {
+  components: {
+    BlogHeader
+  }
+};
+</script>
+
+<style></style>
---
+++ src/components/BlogHeader.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="header">
+    <ul>
+      <li id="list">1{{ msg }}</li>
+      <li id="list">2{{ msg }}</li>
+      <li id="list">3{{ msg }}</li>
+      <li id="list">4{{ msg }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BlogHeader",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scope lang="scss">
+#header {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+  #list {
+    padding: 30px;
+    display: inline;
+    list-style: none;
+  }
+}
+</style>
